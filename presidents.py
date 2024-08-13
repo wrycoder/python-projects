1297d8e891dc7363c2528168a997e20c7c264392
Michael Toulouse <wrycoder@ameritech.net>
2024-08-13 14:27:17 +0000

    Moved utility function to a different place

--- presidents.py
+++ presidents.py
@@ -80,33 +80,6 @@ def ordinal(n: int):
         suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
     return str(n) + suffix
 
-class President:
-    def __init__(self, key, name, year, state):
-        self.key = int(key)
-        self.name = name
-        self.sworn_in = int(year)
-        self.state = states[state]
-
-    def display(self):
-        print("{:*^95}".format(""))
-        message =   "{0}, {1} president of the United States, "\
-                    "was inaugurated in {2}".format(self.name, ordinal(self.key), self.sworn_in)
-        center_and_star(message)
-        message = "{0} was a resident of {1} on Inauguration Day.".format(self.name, self.state)
-        center_and_star(message)
-        print("{:*^95}".format(""))
-
-#
-# 1. Initialize the system
-#
-
-for line in fileinput.input():
-    stats = line.rstrip().split('\t')
-    key = stats[0]
-    p = President(key, stats[1], stats[2], stats[3])
-    presidents[key] = p
-
-
 def for_year(year):
     """Find out who was president in the given year"""
     y = int(year)
@@ -117,12 +90,37 @@ def for_year(year):
             target_key = int(i) - 1
             return presidents[str(target_key)]
 
+
+class President:
+    def __init__(self, key, name, year, state):
+        self.key = int(key)
+        self.name = name
+        self.sworn_in = int(year)
+        self.state = states[state]
+
+    def display(self):
+        message =   "{0}, {1} president of the United States, "\
+                    "was inaugurated in {2}.".format(self.name, ordinal(self.key), self.sworn_in)
+        center_and_star(message)
+        message = "{0} was a resident of {1} on Inauguration Day.".format(self.name, self.state)
+        center_and_star(message)
+
+#
+# 1. Initialize the system
+#
+for line in fileinput.input():
+    stats = line.rstrip().split('\t')
+    key = stats[0]
+    p = President(key, stats[1], stats[2], stats[3])
+    presidents[key] = p
+    presidential_states[stats[3]].append(key)
+
 #
 # 2. Enter an endless loop with a prompt
 #
 while(True):
     result = input(prompt)
-    president = 0
+    president_list = []
     onum = 0
     match result:
         case 'q':
