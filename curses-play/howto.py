df88ad7bce3fba0767ea7abe210831ca0eddeb80
Michael Toulouse <wrycoder@ameritech.net>
2024-09-16 12:16:16 +0000

    Fixing centering on prompt; adding variable length feature

--- curses-play/howto.py
+++ curses-play/howto.py
@@ -2,13 +2,11 @@ import curses
 from curses import wrapper
 import sys
 
-DEBUG_LIMIT = 5
-
 def main(stdscr):
     """Display a multi-page document using a pad and a window"""
-    window_height = curses.LINES - 5 
+    window_height = curses.LINES - 5
     window_width = curses.COLS - 10
-    total_pages = DEBUG_LIMIT
+    total_pages = int(sys.argv[1])
     total_chars = window_height * window_width * total_pages
     current_page = 0
     pad = curses.newpad((window_height * total_pages), window_width)
@@ -29,7 +27,7 @@ def main(stdscr):
             if current_page > 0:
                 menu_message += 'b: backward'
         menu_message += '; q: quit'
-        half_length_of_message = int(len(menu_message))
+        half_length_of_message = int(len(menu_message) / 2)
         p_height, p_width = prompt.getmaxyx()
         p_midpoint = int(p_width / 2)
         x_position = p_midpoint - half_length_of_message
@@ -55,5 +53,12 @@ def main(stdscr):
                 break
             case _:
                 continue
+
+if __name__ == "__main__":
+    try:
+        total_pages = int(sys.argv[1])
+    except(IndexError):
+        print("Please tell me how many pages of data to generate")
+        sys.exit()
 
 wrapper(main)
