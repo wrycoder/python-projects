e7475f93482af4113b4d1477a1592d8e804144a0
Michael Toulouse <wrycoder@ameritech.net>
2024-09-18 19:34:54 +0000

    Update test to work with class

--- paginator_test.py
+++ paginator_test.py
@@ -1,31 +1,66 @@
 import unittest
-from paginator import PaginatorException, load_data, paginate
-import curses
-import re
+from paginator import Paginator, PaginatorException
+import paginator, curses, re, os
 
 class TestPaginator(unittest.TestCase):
 
-    def test_load_data(self):
+    def test_arg_handling(self):
+        p = Paginator()
         try:
             stdscr = curses.initscr()
             self.assertRaises(
-                PaginatorException, 
-                load_data,
-                stdscr
+                PaginatorException,
+                p.load_data,
+                stdscr,
+                'ptest.txt'
             )
-            error_message = "Dude you messed up"
-            err_re = re.compile(error_message)
-            stdscr.clear()
-            stdscr.refresh()
+            err_re = re.compile(paginator.MISSING_SOURCE_MSG)
             self.assertRaisesRegex(
                 PaginatorException,
                 err_re,
-                load_data,
+                p.load_data,
                 stdscr,
-                error_message
+                'ptest.txt'
             )
         finally:
             curses.endwin()
+
+    def test_input_format_errors(self):
+        p = Paginator(True)
+        try:
+            stdscr = curses.initscr()
+            err_re = re.compile(paginator.WRONG_FORMAT_MSG)
+            self.assertRaisesRegex(
+                PaginatorException,
+                err_re,
+                p.load_data,
+                stdscr,
+                'foo.bar'
+            )
+        finally:
+            curses.endwin()
+
+    def test_data_loading(self):
+        p = Paginator(True)
+        data = []
+        line_length = 500
+        total_lines = 500
+        filename = '/home/ubuntu/python-src/test.txt'
+        for y in range(0, total_lines):
+            data.append('')
+            for x in range(0, (line_length - 1)):
+                data[y] += chr(ord('a') + (x*x+y*y) % 26)
+            data[y] += '\n'
+        with open(filename, 'w') as ofile:
+            ofile.writelines(data)
+        try:
+            stdscr = curses.initscr()
+            result = p.load_data(stdscr, filename)
+            self.assertIsInstance(result, list)
+            self.assertIn(data[3], result)
+        finally:
+            curses.endwin()
+            os.remove('test.txt')
 
 if __name__ == '__main__':
     unittest.main()
