b80bb809d4f556a1e2e306ea58c35ee1832a9983
Michael Toulouse <wrycoder@ameritech.net>
2024-10-11 10:12:29 -0400

    Make sure all relevant details are printed

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -37,7 +37,7 @@ class Card:
         else:
             return self.title
 
-    def display(self, template):
+    def display(self, template, topics={}):
         """
         Show this card, using fixed text with markup to be replaced by
         properties of the current subject. The markup syntax for these
@@ -48,11 +48,21 @@ class Card:
             template:   the boilerplate text with embedded symbols
                         (must be contained in a list of at least one
                         element)
+            topics:     dictionary of related topics, which may or
+                        may not contain the subject of the current card
         """
         result = []
         for line in template:
             new_line = re.sub(r'\{card\[', '{self[', line)
             result.append(eval('f"' + new_line + '"'))
+        if len(topics) > 0:
+            for (topic, table) in topics.items():
+                members = table['members']
+                if self.title in members:
+                    topic_template = table['detail']
+                    for topic_line in topic_template:
+                        new_topic_line = re.sub(r'\{card\[', '{self[', topic_line)
+                        result.append(eval('f"' + new_topic_line + '"'))
         return result
 
 class CardEncoder(json.JSONEncoder):
@@ -73,11 +83,11 @@ class Deck:
             self.display_template = js_data['display_template']
             raw_data = js_data['data']
             self.data = []
+            self.topics = {}
             for item in raw_data:
                 self.data.append(Card(item.pop('title'), **item))
             if 'topics' in js_data:
                 topic_list = js_data['topics']
-                self.topics = {}
                 for item in topic_list:
                     self.topics.update(item)
         except(json.decoder.JSONDecodeError) as ex:
@@ -101,13 +111,17 @@ class Deck:
         result = []
         for card in self.data:
             if for_topic is None:
-                result += card.display(self.display_template)
+                result += card.display(
+                    self.display_template,
+                    self.topics
+                )
             elif for_topic in self.topics:
                 members = self.topics[for_topic]['members']
                 if card.title in members:
                     result += card.display(
-                        self.display_template +
-                        self.topics[for_topic]['detail'])
+                        self.display_template,
+                        self.topics
+                    )
         return result
 
 def do_loop(stdscr, cards):
@@ -118,6 +132,12 @@ def do_loop(stdscr, cards):
         curses.curs_set(0)
         height, width = stdscr.getmaxyx()
         paging_win = curses.newwin(height - 1, width - 1, 0, 0)
+        y_index = 0
+        for line in cards.display_all():
+            stdscr.addstr(y_index, 0, line)
+            y_index += 1
+        stdscr.addstr(y_index, 0, 'Press q to quit')
+        stdscr.refresh()
         result = stdscr.getch()
 
 if __name__ == "__main__":
